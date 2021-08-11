91f7ef4a379e4a3f662346c3e78269c71a1e04bf
emrissol <emrissol@gmail.com>
2021-08-11 18:59:25 +0300

    LabelText.java

---
+++ src/com/emrissol/expression/LabelText.java
@@ -0,0 +1,17 @@
+package com.emrissol.expression;
+
+public class LabelText {
+    public static final String ROOT = "root";
+    public static final String NEGATIVE = "+/-";
+    public static final String PARENTHESIS_LEFT = "(";
+    public static final String PARENTHESIS_RIGHT = ")";
+    public static final String PERCENT = "%";
+    public static final String CLEAR = "C";
+    public static final String DEL = "del";
+    public static final String SIGN_DIVIDE = "/";
+    public static final String SIGN_MULTIPLY = "*";
+    public static final String SIGN_PLUS = "+";
+    public static final String SIGN_MINUS = "-";
+    public static final String EQUAL = "=";
+
+}
