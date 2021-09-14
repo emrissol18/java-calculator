71fa0bca5245d0fa58831d101dad7392af0dcf8d
emrissol <emrissol@gmail.com>
2021-09-14 15:48:15 +0300

    refactor SqrtActionListener actionPerformed

--- src/com/emrissol/calc/event/operator/SqrtActionListener.java
+++ src/com/emrissol/calc/event/operator/SqrtActionListener.java
@@ -22,30 +22,30 @@ public class SqrtActionListener extends AbstractOperatorActionListener {
         Expression current = manager.getCurrentExp();
         Expression expression = new Expression();
         expression.getPreOperations().add(new SqrtPreOperation());
-
+        manager.setAndAddCurrentExp(expression);
         // add new expression to current if current is parent (e.g. parent( child( 2 + 2 + [new expression]) ) )
-        if (manager.hasCurrent() && current.isParent()) {
+//        if (manager.hasCurrent() && current.isParent()) {
 
 //            current.addExpression(expression);
 //            manager.setCurrentExp(expression);
 //            manager.addExpressionIfHasNoParent(current);
-            manager.setAndAddCurrentExp(expression);
+//            manager.setAndAddCurrentExp(expression);
 
 //            manager.getCurrentExp().addExpression(expression);
 //            manager.setCurrentExp(expression);
 //            manager.setCurrentParentExp(manager.getCurrentExp());
-        }
+//        }
         // add new expression to current parent (after another expression) (e.g. parent( child( 2 ) + [new expression] )
 //        else if (manager.hasCurrentParent()) {
 //            manager.getCurrentParentExp().addExpression(expression);
 //            manager.setCurrentExp(expression);
 //        }
         // simple add new expression (ancestor in new hierarchy)
-        else {
+//        else {
 //            manager.addExpression(expression);
 //            manager.setCurrentExp(expression);
-            manager.setAndAddCurrentExp(expression);
-        }
+//            manager.setAndAddCurrentExp(expression);
+//        }
 
     }
 
