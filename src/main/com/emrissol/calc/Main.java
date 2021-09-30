06abff5927628739526dab517718e28ff44e07cb
emrissol <emrissol@gmail.com>
2021-09-30 20:44:02 +0300

    1

--- src/main/com/emrissol/calc/Main.java
+++ src/main/com/emrissol/calc/Main.java
@@ -17,20 +17,5 @@ public class Main {
             UIManager uiManager = new UIManager(manager);
         });
     }
-    /*Executable Java Wrappers
-    They take your Java app as input and wrap them in an executable (for a specified platform). 
-    You can customize them as you like; and if the user doesn't have 
-    Java installed, the download page will open.
-
-    Some examples are Launch4J, JSmooth and Jar2EXE.
-     
-
-    Installers
-    They are independent applications configured to copy your app files to the user's 
-    computer and (optionally) create a shortcut.
-
-    Some installers are written in Java, so they're multiplatform. In this case, the installer is a .jar.
-    Some others are platform-dependent, but you have the advantage that you don't need to wrap them.
-
-    Java installers: IzPack, Packlet, PackJacket, Antigen, …*/
+    
 }
